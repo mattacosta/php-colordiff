33c0cd05207e57a3343f393999517bf76e1ac7ac
Matt Acosta <mattacosta@users.noreply.github.com>
2017-11-17 18:03:04 -0700

    Update copyright year.

--- src/ColorDiff.php
+++ src/ColorDiff.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2014 Matt Acosta
+ * Copyright 2017 Matt Acosta
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
